1e40ecc1c884ada8db3cd9e5ef85e12cd968ce5c
Hiroki Honda <cside.story@gmail.com>
2011-03-20 10:31:32 +0900

    initial commit

---
+++ lib/Fetch/WithStatic.pm
@@ -0,0 +1,32 @@
+package Fetch::WithStatic;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Fetch::WithStatic -
+
+=head1 SYNOPSIS
+
+  use Fetch::WithStatic;
+
+=head1 DESCRIPTION
+
+Fetch::WithStatic is
+
+=head1 AUTHOR
+
+Cside E<lt>cside.story@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
